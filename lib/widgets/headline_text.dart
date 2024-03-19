b2daf3ae27cf4478be1110082fca9e5e75e727ea
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-19 20:15:41 +0530

    Add HeadlineText widget

---
+++ lib/widgets/headline_text.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HeadlineText extends StatelessWidget {
+  const HeadlineText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Start Your",
+          style: Theme.of(context)
+              .textTheme
+              .headlineMedium
+              ?.copyWith(fontSize: MediaQuery.of(context).size.width * 0.08),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.01,
+        ),
+        Text(
+          "workout today!",
+          style: Theme.of(context)
+              .textTheme
+              .headlineMedium
+              ?.copyWith(fontSize: MediaQuery.of(context).size.width * 0.08),
+        ),
+      ],
+    );
+  }
+}
