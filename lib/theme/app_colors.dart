fdabb00156ebd6d12b1643b0e01c393638da1900
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-19 19:29:39 +0530

    Add app_colors.dart with color constants

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff643DF2);
+  static const Color green = Color(0xff0D8C15);
+  static const Color bgColor = Colors.black;
+  static const Color blackVar = Color(0xff212126);
+}
