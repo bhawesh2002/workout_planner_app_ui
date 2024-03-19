67c0009c78e5420838fa497a5ced8dda305837da
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-19 19:30:07 +0530

    Add Google Fonts and custom theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:workout_planner_app/pages/home.dart';
+import 'package:workout_planner_app/theme/app_colors.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,8 +12,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      theme: ThemeData(
+          scaffoldBackgroundColor: AppColors.bgColor,
+          primaryColor: AppColors.primary,
+          fontFamily: GoogleFonts.martianMono().fontFamily),
+      home: const HomePage(),
     );
   }
 }
