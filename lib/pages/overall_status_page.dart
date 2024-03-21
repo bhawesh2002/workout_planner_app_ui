1558254cea4f8fe28f1d3a19f96ee1d880ec0615
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-21 19:32:37 +0530

    Update function to handle edge case

--- lib/pages/overall_status_page.dart
+++ lib/pages/overall_status_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:workout_planner_app/theme/app_colors.dart';
 import 'package:workout_planner_app/widgets/current_weight_card.dart';
 import 'package:workout_planner_app/widgets/more_button.dart';
 
@@ -45,6 +46,208 @@ class OverallStatusPage extends StatelessWidget {
               child: const Align(
                 alignment: Alignment.center,
                 child: CurrentWeightCard(),
+              ),
+            ),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: width * 0.9,
+                  height: height * 0.3,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: height * 0.12,
+                          width: width * 0.9,
+                          decoration: BoxDecoration(
+                            color: AppColors.primary,
+                            borderRadius: BorderRadius.circular(width * 0.08),
+                          ),
+                          padding: EdgeInsets.only(
+                            top: height * 0.035,
+                            bottom: height * 0.02,
+                            left: width * 0.065,
+                            right: width * 0.065,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "1.280",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleLarge
+                                          ?.copyWith(
+                                            fontSize: height * 0.03,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                      children: [
+                                        TextSpan(
+                                          text: " Kcal",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleMedium
+                                              ?.copyWith(
+                                                fontWeight: FontWeight.normal,
+                                              ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Text(
+                                    "Target: 2,500 kcal",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleLarge
+                                        ?.copyWith(
+                                          fontSize: height * 0.01,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                              Expanded(
+                                child: SizedBox(
+                                  child: Stack(
+                                    children: [
+                                      Positioned.fill(
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: LinearProgressIndicator(
+                                            value: 1,
+                                            color: Colors.white,
+                                            minHeight: 3,
+                                            borderRadius:
+                                                BorderRadius.circular(width),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned.fill(
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: LinearProgressIndicator(
+                                            value: 0.6,
+                                            color: Colors.black,
+                                            backgroundColor: Colors.transparent,
+                                            minHeight: 10,
+                                            borderRadius:
+                                                BorderRadius.circular(width),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: height * 0.02,
+                        ),
+                        Container(
+                          height: height * 0.12,
+                          width: width * 0.9,
+                          decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(width * 0.08),
+                          ),
+                          padding: EdgeInsets.only(
+                            top: height * 0.035,
+                            bottom: height * 0.02,
+                            left: width * 0.065,
+                            right: width * 0.065,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                      text: "1.725",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleLarge
+                                          ?.copyWith(
+                                            fontSize: height * 0.03,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                      children: [
+                                        TextSpan(
+                                          text: " Steps",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .titleMedium
+                                              ?.copyWith(
+                                                fontWeight: FontWeight.normal,
+                                              ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Text(
+                                    "Target: 8000 steps",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleLarge
+                                        ?.copyWith(
+                                          fontSize: height * 0.01,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                              Expanded(
+                                child: SizedBox(
+                                  child: Stack(
+                                    children: [
+                                      Positioned.fill(
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: LinearProgressIndicator(
+                                            value: 1,
+                                            color: Colors.white,
+                                            minHeight: 3,
+                                            borderRadius:
+                                                BorderRadius.circular(width),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned.fill(
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: LinearProgressIndicator(
+                                            value: 0.6,
+                                            color: AppColors.primary,
+                                            backgroundColor: Colors.transparent,
+                                            minHeight: 10,
+                                            borderRadius:
+                                                BorderRadius.circular(width),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
