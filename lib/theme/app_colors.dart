930bc1995d66700e818bedf617430787826f2483
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-25 09:39:20 +0530

    Add primaryGradient to AppColors

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -5,4 +5,13 @@ class AppColors {
   static const Color green = Color(0xff0D8C15);
   static const Color bgColor = Colors.black;
   static const Color blackVar = Color(0xff212126);
+  static const LinearGradient primaryGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    stops: [0.05, 0.95],
+    colors: <Color>[
+      Color.fromARGB(255, 127, 81, 255),
+      Color.fromARGB(255, 117, 67, 255),
+    ],
+  );
 }
